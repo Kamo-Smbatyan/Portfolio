17531f0b3026441b27344676b6c968bbaca07ad2
Xander Carruth <acarruth2018@gmail.com>
2023-05-26 12:46:04 +0900

    Initial setup commit

---
+++ src/app/components/Main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div className='w-full h-screen text-center'>
+            <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+                <div>
+                    <p>Lorem Ipsum Dolor</p>
+                    <h1 className='py-4 text-gray-700'>Xander Carruth Portfolio Site</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main;
