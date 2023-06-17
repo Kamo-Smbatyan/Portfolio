c20a18943df116afb14ddfdea211bd557c8910e9
Xander Carruth <acarruth2018@gmail.com>
2023-06-17 07:45:31 -0400

    another minor fix

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -12,7 +12,7 @@ const Main = () => {
     }, []);
 
     return (
-        <div id='home' className='w-full h-screen text-center relative bg-[#ecf0f3] overflow-hidden'>
+        <div id='home' className='w-full h-screen text-center relative bg-white overflow-hidden'>
             <div className='w-full h-full text-white mx-auto p-2 flex justify-center items-center absolute z-10'>
                 <div className='w-full'>
                     <h1 className='py-4'>Xander Carruth</h1>
