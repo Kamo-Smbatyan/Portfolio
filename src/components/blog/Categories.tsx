b29a54bc936d293d39b2547de0e1158f8a37b9d6
Xander Carruth <acarruth2018@gmail.com>
2024-06-09 09:57:12 -0400

    Made categories page responsive

--- src/components/blog/Categories.tsx
+++ src/components/blog/Categories.tsx
@@ -9,7 +9,7 @@ type CategoriesProps = {
 
 const Categories: React.FC<CategoriesProps> = ({ categories, currentSlug }) => {
    return (
-      <div className="px-20 mt-10 border-t-2 text-dark border-b-2 border-solid border-dark py-4 flex items-start flex-wrap font-medium mx-10">
+      <div className="px-0 md:px-10 sxl:px-20 mt-10 border-t-2 text-dark border-b-2 border-solid border-dark py-4 flex items-start flex-wrap font-medium mx-5 md:mx-10">
          {categories.map(cat => (
             <Category key={cat} link={`/categories/${cat}`} name={cat} active={currentSlug == slug(cat)} />
          ))}
