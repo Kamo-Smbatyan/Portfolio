669ec28cf0988f72ed43ed76fead5638e6eda7fc
Xander Carruth <acarruth2018@gmail.com>
2024-06-09 21:01:54 -0400

    Fixed scrolling issue for blog home

--- src/app/blog-home/page.tsx
+++ src/app/blog-home/page.tsx
@@ -4,12 +4,17 @@ import FeaturedPosts from '../../components/blog-home/FeaturedPosts';
 import HomeCoverSection from '../../components/blog-home/HomeCoverSection';
 import { allBlogs } from '@contentlayer/generated';
 import RecentPosts from '../../components/blog-home/RecentPosts';
+import { useLayoutEffect } from 'react';
 
 const BlogHome = () => {
+   useLayoutEffect(() => {
+      window.scrollTo(0, 0);
+   });
+
    return (
       <div>
-         {/* <Navbar /> */}
          <div className="flex flex-col items-center justify-center mb-20">
+            <div></div>
             <HomeCoverSection blogs={allBlogs} />
             <FeaturedPosts blogs={allBlogs} />
             <RecentPosts blogs={allBlogs} />
