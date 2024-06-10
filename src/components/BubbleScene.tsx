2fdcea4215e1ab65d7081360a7adf153d76d4f01
Xander Carruth <acarruth2018@gmail.com>
2024-06-09 20:17:14 -0400

    Fixed initial white flash on portfolio load

--- src/components/BubbleScene.tsx
+++ src/components/BubbleScene.tsx
@@ -1,16 +1,19 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { createBubbleScene } from './three/createBubbleScene';
+import { cx } from '@src/utils';
 
 const BubbleScene = () => {
    const containerRef = useRef<HTMLDivElement | null>(null);
    const rendererRef = useRef<THREE.WebGLRenderer | null>(null);
    const cleanupRef = useRef<() => void>(() => {});
+   const [rendered, setRendered] = useState(false);
 
    useEffect(() => {
       if (containerRef.current && !rendererRef.current) {
          const { renderer, cleanup } = createBubbleScene(containerRef.current);
          rendererRef.current = renderer;
          cleanupRef.current = cleanup;
+         setRendered(true);
       }
 
       return () => {
@@ -23,7 +26,9 @@ const BubbleScene = () => {
       };
    }, []);
 
-   return <div className="w-full h-[100vh] absolute z-0 bg-black" ref={containerRef} />;
+   return (
+      <div className={cx('w-full h-[100vh] absolute z-0', rendered ? 'bg-off-white' : 'bg-black')} ref={containerRef} />
+   );
 };
 
 export default BubbleScene;
